3b5164567c9bb2fa7cf9cb8aefdae17ee3081fcf
codefu Master <616841812@qq.com>
2016-11-30 23:04:40 +0800

    火狐浏览器兼容问题解决

---
+++ js/main.js
@@ -0,0 +1,51 @@
+$(function(){
+	$('#main_box').fullpage({
+		sectionsColor: ['#1bbc9b', '#1889C5', '#7BAABE', '#91B493'],
+		anchors: ['page1', 'page2', 'page3', 'page4'],
+		menu:"#menu",
+		afterRender:function(){
+			$('.section1 p, .section1 h1').hide();
+			$('.section1 p, .section1 h1').fadeIn(2000);
+			
+		},
+		afterLoad:function(anchorLink, index){
+			// if(index == 2) {
+			// 	$('.section2').find('h1').fadeIn(2000);
+			// 	$('.personal_details').animate({margin:"0"},1000);
+			// 	$('.individual_skills').animate({marginLeft:"50%"},1000);
+			// }
+			if(index == 3) {
+				$('.section3 h1').animate({marginLeft:"0"}, 1000);
+				$('.section3 h3').animate({marginLeft:"0"}, 1000);
+				$('.section3 div').animate({marginLeft:"0"}, 1000);
+			}
+			if(index == 4) {
+				$('.section4 h1').fadeIn(2000);
+				$(".section4 h3").delay(1000).animate({marginTop:"50px"}, 1000);
+
+			}
+		},
+		onLeave: function(index, direction){
+			// if(index == '2') {
+			// 	$('.section2').find('h1').fadeOut(2000);
+			// 	$('.personal_details').animate({margin:"0 0 0 -100%"},1000);
+			// 	$('.individual_skills').animate({marginLeft:"150%"},1000);
+
+			// }
+			if(index == "3") {
+				$('.section3 h1').animate({marginLeft:"-100%"}, 1000);
+				$('.section3 h3').animate({marginLeft:"-100%"}, 1000);
+				$('.section3 div').animate({marginLeft:"150%"}, 1000);
+			}
+			if(index == "4") {
+				$('.section4 h1').fadeOut(2000);
+				$(".section4 h3").animate({marginTop:"100%"}, 1000);
+			}
+			
+		}
+		
+	})
+	
+	//
+});
+
